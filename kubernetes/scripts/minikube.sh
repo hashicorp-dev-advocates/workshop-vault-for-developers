abae27a6524c1b1dbd48d0e04b2924978d9e488e
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2025-03-31 10:09:53 -0400

    Fix Kubernetes deployment

--- kubernetes/scripts/minikube.sh
+++ kubernetes/scripts/minikube.sh
@@ -5,6 +5,6 @@ set -e -o pipefail
 minikube start --driver=docker
 
 eval $(minikube -p minikube docker-env)
-cd ../payments-database && docker build -t payments-database .
-cd ../payments-processor && docker build -t payments-processor .
-cd ../payments-app/java && docker build -t payments-app:java .
+cd ../payments-database && docker build --no-cache -t payments-database .
+cd ../payments-processor && docker build --no-cache -t payments-processor .
+cd ../payments-app/java && docker build --no-cache -t payments-app:java .
--- payments-database/setup.sql
+++ payments-database/setup.sql
@@ -6,10 +6,7 @@ CREATE TABLE payments (
     name VARCHAR(255) NOT NULL,
     billing_address VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    number VARCHAR(255) NOT NULL,
-    expiration VARCHAR(255) NOT NULL,
-    security_code VARCHAR(255) NOT NULL,
     status VARCHAR(255)
 );
 
-INSERT INTO payments (id, name, billing_address, created_at, number, expiration, security_code, status) VALUES ('2310d6be-0e80-11ed-861d-0242ac120002', 'Red Panda', '8 Eastern Himalayas Drive', CURRENT_DATE, '1234-5678-9012-3456', '10/2026', '987', 'paid');
+INSERT INTO payments (id, name, billing_address, created_at, status) VALUES ('2310d6be-0e80-11ed-861d-0242ac120002', 'Red Panda', '8 Eastern Himalayas Drive', CURRENT_DATE, 'paid');
